2d001c82d66dc6450f014dd3e4dd5452e0c04b0d
Guilherme896 <guilherme.pbp84@gmail.com>
2025-10-06 10:56:46 -0300

    ministro alexandre de moraes
    mini churros alexandre de moraes

--- pires.js
+++ pires.js
@@ -15,6 +15,11 @@ app.use(session({
 app.set('view engine','ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
 app.use(express.urlencoded({extended:true}));
 
 app.use(express.static(path.join(__dirname,'public')));
