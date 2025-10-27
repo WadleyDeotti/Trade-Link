3ceef95540da99265565eb55f9a409302b449af1
WadleyDeotti <wadleyfdjr@gmail.com>
2025-10-27 10:42:56 -0300

    Página de Contratos, editor de contrato usando quill, etc

--- routes/ProdutoRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const produtoController = require('../controllers/CadastroProdutos');
-
-
-// ------------------- POSTs -------------------
-// Cadastro
-router.post('/cadastrar', produtoController.cadastrarProduto);
