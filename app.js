776cad35524c9adcc51da93d65eee32569bb9a22
Guilherme896 <guilherme.pbp84@gmail.com>
2025-11-19 19:08:21 -0300

    s

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of c29565f (lsa)
 
 // Middlewares
 app.use(express.json());
