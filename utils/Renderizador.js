82272399586dd2ef77e1f2415e61e78af0e670dd
Guilherme896 <guilherme.pbp84@gmail.com>
2025-11-27 15:01:11 -0300

    buceta

--- utils/Renderizador.js
+++ utils/Renderizador.js
@@ -1,13 +1,11 @@
 // utils/Renderizador.js
 import CategoriaRenderizador from './CategoriaRenderizador.js';
 import ProdutoRenderizador from './ProdutoRenderizador.js';
-import FornecedorRenderizador from './FornecedorRenderizador.js';
 
 export default class Renderizador {
   constructor() {
     this.categoriaRenderizador = new CategoriaRenderizador();
     this.produtoRenderizador = new ProdutoRenderizador();
-    this.fornecedorRenderizador = new FornecedorRenderizador();
   }
 
   async render(res, view, data = {}) {
@@ -16,8 +14,6 @@ export default class Renderizador {
         return this.categoriaRenderizador.render(res, data);
       case 'produto':
         return this.produtoRenderizador.render(res, data);
-      case 'fornecedores':
-        return await this.fornecedorRenderizador.render(res, data);
       default:
         return res.render(view, data);
     }
