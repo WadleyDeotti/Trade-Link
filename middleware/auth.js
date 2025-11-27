2672e752344cad93fb4ddd5835df9e91538d58b5
Henrique947 <22301658@aluno.cotemig.com.br>
2025-11-27 10:25:05 -0300

    melhorias na proteção

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,6 @@
 export const autenticar = (req, res, next) => {
-  if (!req.session || !req.session.usuario) {
+  if (!req.session.usuario) {
+    req.session.mensagem = "Você precisa estar logado para acessar essa página.";
     return res.redirect("/login");
   }
   next();
